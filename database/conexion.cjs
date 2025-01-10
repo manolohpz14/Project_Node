39c4fb13ddb9bf00801a4f8de5b35e5281689996
Manolo Ordenador <manolohpz14@gmail.com>
2025-01-10 01:53:32 +0100

    12

--- database/conexion.cjs
+++ database/conexion.cjs
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 async function conexion() {
     try {
-        console.log("mongodb://"+process.env.PATH_DB+":27017/ClassProject?ssl=false")
-        await mongoose.connect("mongodb://"+process.env.PATH_DB+":27017/ClassProject?ssl=false");
+        console.log("mongodb://"+process.env.PATH_DB+":27017/ClassProject?ssl=false&tls=false")
+        await mongoose.connect("mongodb://"+process.env.PATH_DB+":27017/ClassProject?ssl=false&tls=false");
         console.log("¡Nos hemos conectado!");
     } catch (error) {  
         console.log(error);
