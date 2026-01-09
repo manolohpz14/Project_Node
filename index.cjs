27ded9f3b46cbecc94f586fb7c65efc3d87494be
manolo <manolohpz14@gmail.com>
2026-01-09 10:17:28 +0100

    nuevo_commit

--- index.cjs
+++ index.cjs
@@ -3,19 +3,19 @@ const express= require("express")
 const cors= require("cors")
 const persona_paths= require("./paths/persona.cjs")
 const cookieParser = require('cookie-parser');
-const fs=require("fs")
-const https = require('http');
+//const fs=require("fs")
+//const https = require('http');
 
 console.log("Iniciamos")
 require("dotenv").config()
 conector.conexion() //Dentro del try se nos printea que estamos conectados
 
 
-const options = {
-    cert: fs.readFileSync('server_combined.pem'),
-    key: fs.readFileSync('server.key'),
-    ca: fs.readFileSync('ca.crt')
-  };
+// const options = {
+//     cert: fs.readFileSync('server_combined.pem'),
+//     key: fs.readFileSync('server.key'),
+//     ca: fs.readFileSync('ca.crt')
+//   };
 
 
 
@@ -27,7 +27,7 @@ app.use(cors()); //me permite conexiones entre dominios cruzados
 
 //Ojo, lo soguiente sirve para que lo que llegue como JSON se transforme como objeto directamente en el req
 //y lo que llegue por express.urleconded se trnasforme directamente a objeto
-app.use(express.json());
+app.use(express.json());``
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -36,18 +36,20 @@ app.use(cookieParser());
 app.use(express.text({ type: "text/plain" })) //Si recibes una petición cuyo Content-Type sea text/plain, entonces léela como texto plano y guárdala en req.body."
 
 
-//app.listen(80, "0.0.0.0", function() {
-//console.log("Escuchando en el puerto 5049 en todas las interfaces");
-//});
+// https.createServer(options, app).listen(5050, () => {
+//     console.log('Servidor HTTPS corriendo en el puerto 443');
+//   });
 
-https.createServer(options, app).listen(5050, () => {
-    console.log('Servidor HTTPS corriendo en el puerto 443');
-  });
+
 // //Rutas
 app.use(persona_paths.router)
 app.use(express.static('public'))
 
 
 
+
+app.listen(5050, "0.0.0.0", () => {
+    console.log("Servidor HTTP corriendo en el puerto 5050");
+});
 
 
